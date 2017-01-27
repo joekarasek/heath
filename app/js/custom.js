4046e6d6b5c68a7ae99d51fe35069d2d0f6a1363
Joe Karasek <joseph.karasek@gmail.com>
2017-01-27 15:26:58 -0800

    add services carousel with background bar

--- app/js/custom.js
+++ app/js/custom.js
@@ -63,8 +63,8 @@
     dots: false,
     margin: 60,
     navText: [
-      '<i class="icon-caret-left"></i>',
-      '<i class="icon-caret-right"></i>'
+      '<img src="assets/arrow%20left.svg" alt="Nav Arrow"></img>',
+      '<img src="assets/arrow%20right.svg" alt="Nav Arrow"></img>'
     ],
     responsive: {
       480: {
